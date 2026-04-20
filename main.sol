9b4d09e60efa78ab3c09c532a2bee58cdc125f43
foxy <jomorgannx@gmail.com>
2026-04-20 14:23:41 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -70,3 +70,75 @@ library FlorStrings {
     function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
         bytes16 symbols = "0123456789abcdef";
         bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; ) {
+            unchecked {
+                buffer[i] = symbols[value & 0xf];
+                value >>= 4;
+                i--;
+            }
+        }
+        return string(buffer);
+    }
+}
+
+library FlorBase64 {
+    bytes internal constant TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return "";
+
+        bytes memory table = TABLE;
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+        bytes memory result = new bytes(encodedLen + 32);
+
+        assembly {
+            let tablePtr := add(table, 1)
+            let resultPtr := add(result, 32)
+            for { let i := 0 } lt(i, mload(data)) { } {
+                i := add(i, 3)
+                let input := and(mload(add(data, i)), 0xffffff)
+                let out := mload(add(tablePtr, and(shr(18, input), 0x3F)))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(input, 0x3F))), 0xFF))
+                out := shl(224, out)
+                mstore(resultPtr, out)
+                resultPtr := add(resultPtr, 4)
+            }
+
+            switch mod(mload(data), 3)
+            case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
+            case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
+
+            mstore(result, encodedLen)
+        }
+
+        return string(result);
+    }
+}
+
+library FlorECDSA {
+    error FlorECDSA_BadSig();
+    error FlorECDSA_BadS();
+    error FlorECDSA_BadV();
+
+    function recover(bytes32 hash, bytes memory signature) internal pure returns (address) {
+        if (signature.length != 65) revert FlorECDSA_BadSig();
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+        // malleability check
+        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            revert FlorECDSA_BadS();
+        }
